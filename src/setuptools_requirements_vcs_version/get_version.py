b63db06cfffa271c1571a8d3332ce12b4b59a8b2
Daniel Brownridge <daniel@freshnewpage.com>
2017-05-29 11:52:55 +0100

    Can retrieve a version. Test passing.

--- src/setuptools_requirements_vcs_version/get_version.py
+++ src/setuptools_requirements_vcs_version/get_version.py
@@ -1,2 +1,23 @@
-def get_version(name, requirments):
-    pass
+"""
+A package intended for use with setuptools that determines the package version
+number by parsing the commit specifier from a VCS URL in a requirements.txt
+file.
+This works assuming a convention that the repository will be tagged as the
+version number and that URL will have the tag as the specifier.
+"""
+from requirements import parse
+
+def get_version(name, reqfile):
+    """
+    Given the name of the package and requirements file, determine the
+    package version.
+    """
+
+    requirements = parse(reqfile)
+
+    for requirement in requirements:
+        if name == requirement.name:
+            return requirement.revision
+
+    return None
+
